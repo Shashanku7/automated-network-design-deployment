65ce9ce261e262c6cb03c50f3adfb7afa7b7dd8c
DevelSSN <shashankshantharamnayak@gmail.com>
2026-06-16 00:08:11 +0530

    Add sample API for Kafka

--- gateway/src/main/java/org/acme/gateway/APIResource.java
+++ gateway/src/main/java/org/acme/gateway/APIResource.java
@@ -1,5 +1,28 @@
 package org.acme.gateway;
 
-import module java.base;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.UUID;
+import org.acme.gateway.services.KafkaService;
 
-public class APIResource {}
+@Path("/api/tasks")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class APIResource {
+
+  @Inject KafkaService kafkaService;
+
+  @POST
+  public Response createTask(CreateTaskRequest request) {
+    kafkaService.sendTask(request.message(), UUID.fromString(request.projectId()));
+
+    return Response.accepted().build();
+  }
+
+  public record CreateTaskRequest(String projectId, String message) {}
+}
